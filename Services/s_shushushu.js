ee152d3adb06fe7f0f202a6146a53d9b369cfb42
ravid.tanami <ravid.tanami@sizmek.com>
2017-10-02 15:23:34 +0300

    services module

--- Services/s_shushushu.js
+++ Services/s_shushushu.js
@@ -6,7 +6,7 @@
     // [] - dependencies from other modules
     var shuApp = angular.module("githubViewer", []);
 
-    var shuMainController = function ($scope, $http) {
+    var shuMainController = function ($scope, $http, $interval, $log) {
 
         var onUserComplete = function(response) {
             $scope.user = response.data;
@@ -17,29 +17,45 @@
         
         var onRepos = function (response) {
             $scope.repos = response.data;
-        }
+        };
 
         var onError = function (reason) {
             $scope.error = "Could not fetch the data"
         };
 
-        /*    $http.get("https://api.github.com/users/ravidgolan")
-         .then(onUserComplete, onError);*/
-        /*$http.get("https://api.github.com/users/ravidgolan")
-            .then(onUserComplete, onError);*/
-
-        $scope.search = function (username) {
-            $http.get("https://api.github.com/users/" + username)
-                .then(onUserComplete, onError);
+        var decrementCountdown = function () {
+            $scope.countdown -= 1;
+            if ($scope.countdown < 1) {
+                $scope.search($scope.username);
+            }
         };
 
+        var countdownInterval = null;
+        var startCountdown = function () {
+            countdownInterval = $interval(decrementCountdown, 1000, $scope.countdown);
+        };
+
+        $scope.search = function (username) {
+            $log.info("Searching for: " + username);
+            $http.get("https://api.github.com/users/" + username)
+                .then(onUserComplete, onError);
+            if (countdownInterval) {
+                $interval.cancel(countdownInterval);
+                $scope.countdown = null;
+            }
+        };
+
+        $scope.username = "angular";
         $scope.message = "GitHub Viewer:)";
         $scope.repoSortOrder = "-stargazers_count";
+        $scope.countdown = 5;
+
+        startCountdown();
     };
 
     shuApp.controller("MainController", shuMainController);
     // for minified:
-    // shuApp.controller("MainController", ["$scope", "$http", shuMainController]);
+    // shuApp.controller("MainController", ["$scope", "$http", "$interval", shuMainController]);
 }());
 
 
