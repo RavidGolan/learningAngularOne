732136d1999559c70bc0b36a257ae35ebbd4425b
ravid.tanami <ravid.tanami@sizmek.com>
2017-10-03 22:32:42 +0300

    using build in directives

--- 2- AngularJS Directives Fundamentals by Joe Eames/3- Using Built-In Directives/ubid_script.js
+++ 2- AngularJS Directives Fundamentals by Joe Eames/3- Using Built-In Directives/ubid_script.js
@@ -11,13 +11,25 @@ angular.module('app').controller('mainCtrl', function ($scope) {
             street: 'PO Box 123',
             city: 'Secret Rabel Base',
             planet: 'Yarin 4'
-        }
+        },
+        friends: [
+            'Han',
+            'Leia',
+            'Chewbacca'
+        ]
+    };
+
+    $scope.userWithoutAddress = {
+        name: 'Ravid Golan'
     }
 });
 
 angular.module('app').directive('userInfoCard', function () {
     return {
-        template: "Name: {{user.name}} <br /> Address: <br />{{user.address.street}} <br />{{user.address.city}}<br />{{user.address.planet}}",
+        // template: "Name: {{user.name}} <br /> <div ng-show='userWithoutAddress.address'>Address: <br />{{user.address.street}} <br />{{user.address.city}}<br />{{user.address.planet}}</div>",
+        template: "Name: {{user.name}} <br /><br /> " +
+                  "<div ng-show='user.address'>Address: <br />{{user.address.street}} <br />{{user.address.city}}<br />{{user.address.planet}}</div><br />" +
+                  "<div>Friends: <div ng-repeat='friend in user.friends'>{{friend}}</div></div>",
         restrict: "E"
     }
 });
