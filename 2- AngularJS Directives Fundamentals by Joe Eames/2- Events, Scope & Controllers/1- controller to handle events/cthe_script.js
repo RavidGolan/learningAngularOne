ade0b5276a246e1b138f31460530e2c25e45ba78
ravid.tanami <ravid.tanami@sizmek.com>
2017-10-03 23:59:19 +0300

    2- events, scpoe & controllers

---
+++ 2- AngularJS Directives Fundamentals by Joe Eames/2- Events, Scope & Controllers/1- controller to handle events/cthe_script.js
@@ -0,0 +1,32 @@
+/**
+ * Created by ravid.tanami on 03/10/2017.
+ */
+
+angular.module('app', []);
+
+angular.module('app').controller('mainCtrl', function ($scope) {
+    $scope.user = {
+        name: 'Luke Skywalker',
+        address: {
+            street: 'PO Box 123',
+            city: 'Secret Rabel Base',
+            planet: 'Yarin 4'
+        },
+        friends: [
+            'Han',
+            'Leia',
+            'Chewbacca'
+        ]
+    };
+
+    $scope.userWithoutAddress = {
+        name: 'Ravid Golan'
+    }
+});
+
+angular.module('app').directive('userInfoCard', function () {
+    return {
+        templateUrl: "cthe_userInfoCard.html",
+        restrict: "E"
+    }
+});
