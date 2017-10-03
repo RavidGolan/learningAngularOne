ffc2cb998885ab772f098d909ed84cd011805cf5
ravid tanami <ravid.tanami@sizmek.com>
2017-10-03 11:38:25 +0300

    ng-include added

---
+++ Routing/s_ghs_github.js
@@ -0,0 +1,30 @@
+/**
+ * Created by ravid.tanami on 2 Oct 2017.
+ */
+(function () {
+    var shugithub = function ($http) {
+
+        var getUser = function (username) {
+            return $http.get("https://api.github.com/users/" + username)
+                .then(function (response) {
+                    return response.data;
+                });
+        };
+
+        var getRepos = function (user) {
+            return $http.get(user.repos_url)
+                .then(function (response) {
+                    return response.data;
+                })
+        };
+
+        return {
+            getUser: getUser,
+            getRepos: getRepos
+        };
+    };
+
+    var module = angular.module("githubViewer");
+    module.factory("github", shugithub);
+
+}());
