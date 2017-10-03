deade942950818286d5a5c4659382823cd248c62
ravid.tanami <ravid.tanami@sizmek.com>
2017-10-03 23:41:25 +0300

    the replace attribute

---
+++ 2- AngularJS Directives Fundamentals by Joe Eames/5- The Replace Attrbute/tra_script.js
@@ -0,0 +1,33 @@
+/**
+ * Created by ravid.tanami on 03/10/2017.
+ */
+
+angular.module('app', []);
+
+angular.module('app').controller('mainCtrl', function ($scope) {
+    $scope.user = {
+        name: 'Luke Skywalker',
+        address: {
+            street: 'PO Box 123',
+            city: 'Secret Rabel Base',
+            planet: 'Yarin 4'
+        },
+        friends: [
+            'Han',
+            'Leia',
+            'Chewbacca'
+        ]
+    };
+
+    $scope.userWithoutAddress = {
+        name: 'Ravid Golan'
+    }
+});
+
+angular.module('app').directive('userInfoCard', function () {
+    return {
+        templateUrl: "tra_userInfoCard.html",
+        restrict: "E",
+        replace: true
+    }
+});
