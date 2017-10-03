e2811706e247ad0890ddf6b533b16f7bfb6512e2
ravid.tanami <ravid.tanami@sizmek.com>
2017-10-03 22:08:58 +0300

    user info card directive

--- 2- AngularJS Directives Fundamentals by Joe Eames/1- User Info Card Directive/uicd_script.js
+++ 2- AngularJS Directives Fundamentals by Joe Eames/1- User Info Card Directive/uicd_script.js
@@ -7,3 +7,10 @@ angular.module('app', []);
 angular.module('app').controller('mainCtrl', function ($scope) {
 
 });
+
+angular.module('app').directive('userInfoCard', function () {
+    return {
+        template: "User Info Card",
+        restrict: "E"
+    }
+});
