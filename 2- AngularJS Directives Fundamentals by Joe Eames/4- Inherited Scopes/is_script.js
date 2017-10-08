cfe570919703d8d103923ffe742e69a68cc02532
ravid tanami <ravid.tanami@sizmek.com>
2017-10-08 17:31:44 +0300

    inherited scopes directory

---
+++ 2- AngularJS Directives Fundamentals by Joe Eames/4- Inherited Scopes/is_script.js
@@ -0,0 +1,71 @@
+/**
+ * Created by ravid.tanami on 03/10/2017.
+ */
+
+angular.module('app', []);
+
+angular.module('app').controller('mainCtrl', function ($scope) {
+    /*$scope.user = {
+        name: 'Luke Skywalker',
+        address: {
+            street: 'PO Box 123',
+            city: 'Secret Rabel Base',
+            planet: 'Yarin 4'
+        },
+        friends: [
+            'Han',
+            'Leia',
+            'Chewbacca'
+        ]
+    };*/
+
+    $scope.user1 = {
+        name: 'Luke Skywalker',
+        address: {
+            street: 'PO Box 123',
+            city: 'Secret Rabel Base',
+            planet: 'Yarin 4'
+        },
+        friends: [
+            'Han',
+            'Leia',
+            'Chewbacca'
+        ]
+    };
+
+    $scope.user2 = {
+        name: 'Han Solo',
+        address: {
+            street: 'PO Box 123',
+            city: 'Mos Eisley',
+            planet: 'Tattione'
+        },
+        friends: [
+            'Han',
+            'Leia',
+            'Chewbacca'
+        ]
+    };
+
+    $scope.userWithoutAddress = {
+        name: 'Ravid Golan'
+    }
+
+    /*$scope.knightMe = function (user) {
+        user.rank = "knight";
+    }*/
+});
+
+angular.module('app').directive('userInfoCard', function () {
+    return {
+        templateUrl: "is_userInfoCard.html",
+        restrict: "E",
+        // scope: true, // inherited scope
+        scope: { user: '='}, // isolated scope
+        controller: function ($scope) {
+            $scope.knightMe = function (user) {
+                user.rank = "knight";
+            }
+        }
+    }
+});
