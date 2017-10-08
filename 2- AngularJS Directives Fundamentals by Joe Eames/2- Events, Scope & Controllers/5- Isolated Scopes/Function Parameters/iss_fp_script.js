3a9c30602dcc1125412b950b2ace2cc60236de14
ravid tanami <ravid.tanami@sizmek.com>
2017-10-08 20:01:53 +0300

    isolated scopes with function parameter

---
+++ 2- AngularJS Directives Fundamentals by Joe Eames/2- Events, Scope & Controllers/5- Isolated Scopes/Function Parameters/iss_fp_script.js
@@ -0,0 +1,127 @@
+/**
+ * Created by ravid.tanami on 03/10/2017.
+ */
+
+angular.module('app', []);
+
+angular.module('app').controller('mainCtrl', function ($scope) {
+    /*$scope.user = {
+        name: 'Luke Skywalker',
+        address: {
+            street: 'PO Box 123',
+            city: 'Secret Rabel Base',
+            planet: 'Yarin 4'
+        },
+        friends: [
+            'Han',
+            'Leia',
+            'Chewbacca'
+        ]
+    };*/
+
+    $scope.user1 = {
+        name: 'Luke Skywalker',
+        address: {
+            street: 'PO Box 123',
+            city: 'Secret Rabel Base',
+            planet: 'Yarin 4'
+        },
+        friends: [
+            'Han',
+            'Leia',
+            'Chewbacca'
+        ]
+    };
+
+    $scope.user2 = {
+        name: 'Han Solo',
+        address: {
+            street: 'PO Box 123',
+            city: 'Mos Eisley',
+            planet: 'Tattione'
+        },
+        friends: [
+            'Han',
+            'Leia',
+            'Chewbacca'
+        ]
+    };
+
+    $scope.userWithoutAddress = {
+        name: 'Ravid Golan'
+    }
+
+    /*$scope.knightMe = function (user) {
+        user.rank = "knight";
+    }*/
+});
+
+angular.module('app').directive('userInfoCard', function () {
+    return {
+        templateUrl: "iss_fp_userInfoCard.html",
+        restrict: "E",
+        // scope: true, // inherited scope
+        // isolated scope
+        scope: {
+            user: '=',
+            /*initialCollapsed: '@',*/
+            initialCollapsed: '@collapsed'
+        },
+        controller: function ($scope) {
+            // $scope.collapsed = false;
+            $scope.collapsed = ($scope.initialCollapsed === 'true');
+            $scope.knightMe = function (user) {
+                user.rank = "knight";
+            };
+
+            $scope.collapse = function () {
+                $scope.collapsed = !$scope.collapsed;
+            };
+
+            $scope.removing = false;
+            $scope.startRemove = function () {
+                $scope.removing = true;
+            };
+            $scope.cancelRemove = function () {
+                $scope.removing = false;
+            };
+            $scope.removeFriend = function (friend) {
+                var idx = $scope.user.friends.indexOf(friend);
+                if (idx > -1) {
+                    $scope.user.friends.splice(idx, 1);
+                }
+            }
+        }
+        /*controller: function ($scope) {
+            // $scope.collapsed = false;
+            $scope.collapsedState = ($scope.collapsed === 'true');
+            $scope.knightMe = function (user) {
+                user.rank = "knight";
+            };
+
+            $scope.collapse = function () {
+                $scope.collapsedState = !$scope.collapsedState;
+            }
+        }*/
+    }
+});
+
+angular.module('app').directive('address', function () {
+    return {
+        templateUrl: 'iss_fp_address.html',
+        restrict: 'E',
+        scope: true,
+        // scope: {}, // we can't do that because the user is no longer visible to this address
+        controller: function ($scope) {
+            $scope.collapsed = false;
+
+            $scope.expand = function () {
+                $scope.collapsed = false;
+            };
+
+            $scope.collapse = function () {
+                $scope.collapsed = true;
+            };
+        }
+    }
+});
