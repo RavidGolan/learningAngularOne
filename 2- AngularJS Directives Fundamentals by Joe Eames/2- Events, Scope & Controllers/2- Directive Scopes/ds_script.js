7e16aceb6c91e03c30ff6cbaa51cf57f4e859819
ravid tanami <ravid.tanami@sizmek.com>
2017-10-08 14:16:44 +0300

    directive scopes: shared (default), inherited & isolated

--- 2- AngularJS Directives Fundamentals by Joe Eames/2- Events, Scope & Controllers/2- Directive Scopes/ds_script.js
+++ 2- AngularJS Directives Fundamentals by Joe Eames/2- Events, Scope & Controllers/2- Directive Scopes/ds_script.js
@@ -19,6 +19,34 @@ angular.module('app').controller('mainCtrl', function ($scope) {
         ]
     };
 
+    $scope.user1 = {
+        name: 'Luke Skywalker',
+        address: {
+            street: 'PO Box 123',
+            city: 'Secret Rabel Base',
+            planet: 'Yarin 4'
+        },
+        friends: [
+            'Han',
+            'Leia',
+            'Chewbacca'
+        ]
+    };
+
+    $scope.user2 = {
+        name: 'Han Solo',
+        address: {
+            street: 'PO Box 123',
+            city: 'Mos Eisley',
+            planet: 'Tattione'
+        },
+        friends: [
+            'Han',
+            'Leia',
+            'Chewbacca'
+        ]
+    };
+
     $scope.userWithoutAddress = {
         name: 'Ravid Golan'
     }
@@ -32,6 +60,8 @@ angular.module('app').directive('userInfoCard', function () {
     return {
         templateUrl: "ds_userInfoCard.html",
         restrict: "E",
+        // scope: true, // inherited scope
+        scope: { user: '='}, // isolated scope
         controller: function ($scope) {
             $scope.knightMe = function (user) {
                 user.rank = "knight";
