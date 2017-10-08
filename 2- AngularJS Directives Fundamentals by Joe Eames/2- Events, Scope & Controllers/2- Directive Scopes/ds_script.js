d55398ab8fdd7f826598ca0c975564c838e9e21e
ravid tanami <ravid.tanami@sizmek.com>
2017-10-08 11:53:21 +0300

    directive scopes directory

---
+++ 2- AngularJS Directives Fundamentals by Joe Eames/2- Events, Scope & Controllers/2- Directive Scopes/ds_script.js
@@ -0,0 +1,41 @@
+/**
+ * Created by ravid.tanami on 03/10/2017.
+ */
+
+angular.module('app', []);
+
+angular.module('app').controller('mainCtrl', function ($scope) {
+    $scope.user = {
+        name: 'Luke Skywalker',
+        address: {
+            street: 'PO Box 123',
+            city: 'Secret Rabel Base',
+            planet: 'Yarin 4'
+        },
+        friends: [
+            'Han',
+            'Leia',
+            'Chewbacca'
+        ]
+    };
+
+    $scope.userWithoutAddress = {
+        name: 'Ravid Golan'
+    }
+
+    /*$scope.knightMe = function (user) {
+        user.rank = "knight";
+    }*/
+});
+
+angular.module('app').directive('userInfoCard', function () {
+    return {
+        templateUrl: "ds_userInfoCard.html",
+        restrict: "E",
+        controller: function ($scope) {
+            $scope.knightMe = function (user) {
+                user.rank = "knight";
+            }
+        }
+    }
+});
