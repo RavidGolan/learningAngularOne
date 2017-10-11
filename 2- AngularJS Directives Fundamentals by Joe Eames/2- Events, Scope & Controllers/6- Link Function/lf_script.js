e9d33acc4d1173cdce7a938160e82cf9d6f2288b
ravid.tanami <ravid.tanami@sizmek.com>
2017-10-11 14:51:07 +0300

    6- Link Function

--- 2- AngularJS Directives Fundamentals by Joe Eames/2- Events, Scope & Controllers/6- Link Function/lf_script.js
+++ 2- AngularJS Directives Fundamentals by Joe Eames/2- Events, Scope & Controllers/6- Link Function/lf_script.js
@@ -8,15 +8,40 @@ angular.module('app').controller('mainCtrl', function ($scope) {
     
 });
 
-angular.module('app').directive('scapebarSupport', function () {
+/*angular.module('app').directive('scapebarSupport', function () {
     return {
         restrict: 'A', //default
-        Link: function(scope, el, attrs) {
+        link: function(scope, el, attrs) {
             $('body').on('keypress', function (evt) {
-                if (evt.keyCode === 32) {
+                var vidEl = el[0];
 
+                if (evt.keyCode === 32) {
+                    if (vidEl.paused) {
+                        vidEl.play();
+                    }
+                    else {
+                        vidEl.pause();
+                    }
                 }
             })
         }
+    }
+});*/
+
+// its the same!
+angular.module('app').directive('scapebarSupport', function () {
+    return function (scope, el, attrs) {
+        $('body').on('keypress', function (evt) {
+            var vidEl = el[0];
+
+            if (evt.keyCode === 32) {
+                if (vidEl.paused) {
+                    vidEl.play();
+                }
+                else {
+                    vidEl.pause();
+                }
+            }
+        })
     }
 });
